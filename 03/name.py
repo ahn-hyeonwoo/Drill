fec63497684e91db258d6faece2b707eb7b87313
ahn-hyeonwoo <90032814+ahn-hyeonwoo@users.noreply.github.com>
2021-09-09 18:36:58 +0900

    drill 03
    2020182019 안현우

---
+++ 03/name.py
@@ -0,0 +1,89 @@
+import turtle
+
+turtle.circle(20)
+
+turtle.penup()
+turtle.goto(30,40)
+turtle.right(90)
+turtle.pendown()
+
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(30,20)
+turtle.left(90)
+turtle.pendown()
+
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(0, -10)
+turtle.right(90)
+turtle.pendown()
+
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(80, 50)
+turtle.pendown()
+
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(70, 45)
+turtle.pendown()
+
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(90,10)
+turtle.pendown()
+
+turtle.circle(15)
+
+turtle.penup()
+turtle.goto(110,35)
+turtle.pendown()
+
+turtle.forward(15)
+
+turtle.penup()
+turtle.goto(110, 20)
+turtle.pendown()
+
+turtle.forward(15)
+
+turtle.penup()
+turtle.goto(125, 50)
+turtle.right(90)
+turtle.pendown()
+
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(90, -10)
+turtle.pendown()
+
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(170, 10)
+turtle.pendown()
+
+turtle.circle(20)
+
+turtle.penup()
+turtle.goto(140, -10)
+turtle.pendown()
+
+turtle.forward(60)
+turtle.backward(30)
+turtle.right(90)
+turtle.forward(25)
+
+
+turtle.exitonclick()
